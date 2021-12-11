7c21cd47c2019ef1e5d68d9ef25d121a9ca7736c
Fred Zhen <13911239075@163.com>
2021-12-11 18:26:59 +0800

    Update main.js with routing, and added new assets, LoginBox, NotFount, Production Item components

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '../node_modules/simple-line-icons/css/simple-line-icons.css'
 import '../node_modules/bulma/css/bulma.min.css';
 import { createApp } from 'vue';
 import App from "./App";
+import router from './router';
 import store from './store';
 
-createApp(App).use(store).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
